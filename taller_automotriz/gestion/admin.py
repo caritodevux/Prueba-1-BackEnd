04476973b091b0cfeca110ef22ba010b1a162366
Carolina <c.rehbeindelrio@gmail.com>
2026-09-04 20:01:48 -0400

    Creación de datos a la base de datos

--- taller_automotriz/gestion/admin.py
+++ taller_automotriz/gestion/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Cliente, Vehiculo, TipoServicio, Servicio
 
-# Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('rut', 'nombre', 'apellido', 'telefono')
+    search_fields = ('rut', 'apellido')
+
+@admin.register(Vehiculo)
+class VehiculoAdmin(admin.ModelAdmin):
+    list_display = ('patente', 'marca', 'modelo', 'cliente')
+    # Habilita un buscador por patente o RUT del dueño
+    search_fields = ('patente', 'cliente__rut') 
+
+@admin.register(TipoServicio)
+class TipoServicioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio_referencial')
+
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('vehiculo', 'fecha', 'estado', 'costo_total')
+    list_filter = ('estado', 'fecha')
+    # Transforma la relación N:M en un selector de doble caja muy fácil de usar
+    filter_horizontal = ('tipos_de_servicio',)
